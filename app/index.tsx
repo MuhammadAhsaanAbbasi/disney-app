610f59ea0121cdcff1904dc656ba32af14dc53d9
MuhammadAhsaanAbbasi <mahsaanabbasi@gmail.com>
2025-08-25 14:27:46 +0500

    Set Project Structure and Styling

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className="flex-1 items-center justify-center"
+    >
+      <Text className="text-5xl text-red-500">
+        Hijab Rabail Khan
+      </Text>
+    </View>
+  );
+}
